8ba3601d96ee94ab9d31468a958c48b8259c1e2b
phil002109 <vermontphil.paguiligan@gmail.com>
2023-10-11 11:12:44 +0800

    Added external functions

--- src/EscrowPayment.sol
+++ src/EscrowPayment.sol
@@ -3,5 +3,11 @@
 pragma solidity 0.8.18;
 
 contract EscrowPayment {
-    
+    function deposit() external {}
+    function withdraw() external {}
+    function receiveProduct() external {}
+    function cancel() external {}
+    function approveDispute() external {}
+    function rejectDispute() external {}
+    function receiveReturnedProduct() external {}
 }
