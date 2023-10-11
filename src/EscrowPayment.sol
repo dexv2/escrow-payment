5c4a4ba28ed601e2fe2b1b26422bcbd93ef14aec
phil002109 <vermontphil.paguiligan@gmail.com>
2023-10-11 11:15:11 +0800

    Added state variables

--- src/EscrowPayment.sol
+++ src/EscrowPayment.sol
@@ -3,6 +3,12 @@
 pragma solidity 0.8.18;
 
 contract EscrowPayment {
+    address private s_buyer;
+    address private s_seller;
+    address private s_deliveryDriver;
+    mapping (address depositor => uint256 amountWithdrawable) s_amountWithdrwable;
+    mapping (address depositor => uint256 amountDeposit) s_amountDeposit;
+
     function deposit() external {}
     function withdraw() external {}
     function receiveProduct() external {}
