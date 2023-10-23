473a6f736e20ffb8ef3f42e096d03ce183d9fa3f
phil002109 <vermontphil.paguiligan@gmail.com>
2023-10-23 17:08:12 +0800

    Added labels and s_depositorsCount state variable

--- src/EscrowPayment.sol
+++ src/EscrowPayment.sol
@@ -5,6 +5,9 @@ pragma solidity 0.8.18;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EscrowPayment {
+    /////////////////
+    // Errors      //
+    /////////////////
     error EscrowPayment__NotABuyer();
     error EscrowPayment__NotASeller();
     error EscrowPayment__NotADeliveryDriver();
@@ -13,15 +16,26 @@ contract EscrowPayment {
     error EscrowPayment__SellerAlreadyDeposited();
     error EscrowPayment__DeliveryDriverAlreadyDeposited();
 
+    ////////////////
+    // Enums      //
+    ////////////////
     enum DepositorType {BUYER, SELLER, DELIVERY_DRIVER};
 
+    //////////////////////////
+    // State Variables      //
+    //////////////////////////
     address private s_buyer;
     address private s_seller;
     address private s_deliveryDriver;
     address private s_tokenSelected;
     uint256 private s_price;
+    uint8 private s_depositorsCount;
     mapping (address depositor => uint256 amountWithdrawable) s_amountWithdrwable;
-    mapping (address depositor => uint256 amountDeposit) s_amountDeposit;
+    mapping (address depositor => bool deposited) s_deposited;
+
+    ////////////////////
+    // Functions      //
+    ////////////////////
 
     constructor(uint256 price, address tokenSelected, DepositorType depositorType) {
         s_price = price;
@@ -29,6 +43,10 @@ contract EscrowPayment {
 
         deposit(depositorType);
     }
+
+    ////////////////////
+    // Modifiers      //
+    ////////////////////
 
     modifier onlyBuyer() {
         if (msg.sender != s_buyer) {
@@ -50,6 +68,10 @@ contract EscrowPayment {
         }
         _;
     }
+
+    /////////////////////////////
+    // External Functions      //
+    /////////////////////////////
 
     function deposit(DepositorType depositorType) public {
         if (depositorType == DepositorType.BUYER) {
@@ -80,24 +102,31 @@ contract EscrowPayment {
 
     function receiveReturnedProduct() external onlySeller {}
 
+    ////////////////////////////
+    // Private Functions      //
+    ////////////////////////////
+
     function _depositAsBuyer() private {
         if (s_buyer != address(0)) {
             revert EscrowPayment__BuyerAlreadyDeposited();
         }
-        s_buyer = msg.sender();
+        s_buyer = msg.sender;
+        s_depositorsCount++;
     }
 
     function _depositAsSeller() private {
         if (s_seller != address(0)) {
             revert EscrowPayment__SellerAlreadyDeposited();
         }
-        s_seller = msg.sender();
+        s_seller = msg.sender;
+        s_depositorsCount++;
     }
 
     function _depositAsDeliveryDriver() private {
         if (s_deliveryDriver != address(0)) {
             revert EscrowPayment__DeliveryDriverAlreadyDeposited();
         }
-        s_deliveryDriver = msg.sender();
+        s_deliveryDriver = msg.sender;
+        s_depositorsCount++;
     }
 }
