2f7dd1277c07e6334e6f6d69d2316f873460dbac
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-03 09:55:08 +0800

    Added HelperConfig contract for multichain integration

---
+++ script/HelperConfig.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
+import {STCFaucet} from "../test/mocks/FaucetMock.sol";
+
+contract HelperConfig is Script {
+    
+}
