3b90cc72fb2817a24f02afda31d0166dcddf8898
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-06 08:39:45 +0800

    Added helper config for anvil

--- script/HelperConfig.sol
+++ script/HelperConfig.sol
@@ -7,5 +7,26 @@ import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 import {STCFaucet} from "../test/mocks/FaucetMock.sol";
 
 contract HelperConfig is Script {
-    
+    NetworkConfig public activeNetworkConfig;
+    uint256 public constant DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
+    struct NetworkConfig {
+        address[] supportedStablecoins;
+        uint256 deployerKey;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaConfig();
+        }
+        else {
+            activeNetworkConfig = getOrCreateAnvilNetworkConfig();
+        }
+    }
+
+    function getOrCreateAnvilNetworkConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
+        if (activeNetworkConfig.supportedStablecoins.length > 0) {
+            return activeNetworkConfig;
+        }
+    }
 }
