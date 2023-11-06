8175578be3880a902509b9d2a9a1184702a9a963
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-06 10:07:00 +0800

    Update DeployFactory script

--- script/DeployFactory.sol
+++ script/DeployFactory.sol
@@ -7,5 +7,15 @@ import {EscrowFactory} from "../src/EscrowFactory.sol";
 import {HelperConfig} from "./HelperConfig.sol";
 
 contract DeployFactory is Script {
+    function run() public returns (EscrowFactory) {
+        HelperConfig config = new HelperConfig();
 
+        uint256 inconvenienceThreshold = 50;
+
+        vm.startBroadcast(config.deployerKey());
+        EscrowFactory factory = new EscrowFactory(config.getSupportedStablecoins(), inconvenienceThreshold);
+        vm.stopBroadcast();
+
+        return factory;
+    }
 }
