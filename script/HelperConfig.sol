0cf8b6bb38773c76d79a8ed8a951a38f345b9c7f
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-06 09:09:34 +0800

    Remove faucet import

--- script/HelperConfig.sol
+++ script/HelperConfig.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
-import {STCFaucet} from "../test/mocks/FaucetMock.sol";
 
 contract HelperConfig is Script {
     NetworkConfig public activeNetworkConfig;
