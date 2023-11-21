9f33dbc57ac807ba5641a7f45933c57bff4c37b5
phil002109 <vermontphil.paguiligan@gmail.com>
2023-11-21 08:45:07 +0800

    Add PhilippinePeso contract to deploy script

--- script/DeployFactory.sol
+++ script/DeployFactory.sol
@@ -4,16 +4,16 @@ pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {EscrowFactory} from "../src/EscrowFactory.sol";
-import {HelperConfig} from "./HelperConfig.sol";
+import {PhilippinePeso} from "../src/PhilippinePeso.sol";
 
 contract DeployFactory is Script {
     function run() public returns (EscrowFactory) {
-        HelperConfig config = new HelperConfig();
-
         uint256 inconvenienceThreshold = 50;
 
-        vm.startBroadcast(config.deployerKey());
-        EscrowFactory factory = new EscrowFactory(config.getSupportedStablecoins(), inconvenienceThreshold);
+        vm.startBroadcast();
+        PhilippinePeso philippinePeso = new PhilippinePeso(msg.sender);
+        EscrowFactory factory = new EscrowFactory(address(philippinePeso), inconvenienceThreshold);
+        philippinePeso.transferOwnership(address(factory));
         vm.stopBroadcast();
 
         return factory;
